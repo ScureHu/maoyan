5963c1dac9ce34534c7aa941cdc322fbe5082556
huruilei <chh15068288171@outlook.com>
2020-06-03 20:06:28 +0800

    Merge branch 'master' of /Users/huruilei/IdeaProjects/maoyan with conflicts.

--- guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
@@ -1,10 +1,5 @@
 package com.stylefeng.guns.rest.modular.film;
 
-import com.alibaba.dubbo.config.annotation.Reference;
-import com.huruilei.film.api.ProducerServiceApi;
-import com.huruilei.film.vo.ImgVO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,26 +12,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/film/")
 public class FilmController {
-
-    @Reference(interfaceClass = ProducerServiceApi.class)
-    private ProducerServiceApi producerServiceApi;
-
-    @GetMapping(value = "msg/{name}")
-    public String send(@PathVariable(name = "name") String msg){
-        producerServiceApi.send(msg);
-        return "SUCCESS";
-    }
-
-
-    @GetMapping(value = "pojo")
-    public String sendPojo(){
-        ImgVO imgVO = new ImgVO();
-        imgVO.setImg01("123");
-        imgVO.setImg02("123555");
-        imgVO.setImg03("12344");
-        imgVO.setImg04("ceshi");
-        imgVO.setMainImg("123123ddddd");
-        producerServiceApi.sendPojo(imgVO);
-        return "SUCCESS";
-    }
 }
